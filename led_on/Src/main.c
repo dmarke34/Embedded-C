781f35bc22629f414aafd25ee9dfa06fce97336b
dmarke34 <ajaparicio@cpp.edu>
2022-11-10 10:02:22 -0800

    One LED (Bitwise Operators)

--- led_on/Src/main.c
+++ led_on/Src/main.c
@@ -16,18 +16,18 @@ int main(void)
 	uint32_t *pPortDModeReg = (uint32_t*)0x40020C00;
 	uint32_t *pPortDOutReg = (uint32_t*)0x40020C14;
 
-	//1. enable the clock for GPOID peripheral in the AHB1ENR
+	//1. enable the clock for GPOID peripheral in the AHB1ENR (SET the 3rd bit position)
 
-	*pClkCtrlReg |= 0x08;
+	*pClkCtrlReg |= (1 << 3);
 
 	//2. configure the mode of the IO pin as output
-	//a. clear the 24th and 25th bit positions
-	*pPortDModeReg &= 0x00FFFFFF;
+	//a. clear the 24th and 25th bit positions (CLEAR)
+	*pPortDModeReg &= ~(3 << 24);
 	//b. make 24th bit position as 1 (SET)
-	*pPortDModeReg |= 0x55000000;
+	*pPortDModeReg |= (1 << 24);
 
 	//3. SET 12th bit of the output data register to make I/O pin-12 HIGH
-	*pPortDOutReg |= 0xF000;
+	*pPortDOutReg |= (1 << 12);
 
 
 	while(1);
